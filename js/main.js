d7a610e38fc90efa5c0bf890a84922094f6fca88
Cristofer Martinez <cristoferm@gmail.com>
2021-05-25 13:20:23 -0700

    issue-1-create-an-entry | cleaned, updated from, updated data.js

--- js/main.js
+++ js/main.js
@@ -4,28 +4,20 @@
 var $photoURL = document.querySelector('.photoURL');
 var $newEntryImg = document.querySelector('.newEntryImg');
 
-// console.log($form.elements.target);
-// console.dir($form);
-// console.log($submit);
-
 $photoURL.addEventListener('input', function (event) {
   $newEntryImg.setAttribute('src', event.target.value);
   if (event.target.value.length === 0) { $newEntryImg.setAttribute('src', 'images/placeholder-image-square.jpg'); }
 });
 
-var $form = document.querySelector('.newEntry');
+var $form = document.querySelector('form');
 
-var $submit = document.querySelector('.button');
-$submit.addEventListener('click', function (event) {
+$form.addEventListener('submit', function (event) {
   event.preventDefault();
-  var title = document.querySelector('.title').value;
-  var photoURL = document.querySelector('.photoURL').value;
-  var notes = document.querySelector('.notes').value;
 
   var newEntry = {
-    title: title,
-    photoURL: photoURL,
-    notes: notes,
+    title: $form.elements.title.value,
+    photoURL: $form.elements.photoURL.value,
+    notes: $form.elements.notes.value,
     ID: data.nextEntryId
   };
 
@@ -34,13 +26,4 @@ $submit.addEventListener('click', function (event) {
 
   $form.reset();
   $newEntryImg.setAttribute('src', 'images/placeholder-image-square.jpg');
-
-  var dataJSON = JSON.stringify(data);
-  var entriesJSON = JSON.stringify(data.entries);
-
-  localStorage.setItem('data', dataJSON);
-  localStorage.setItem('entries', entriesJSON);
-
-  // console.log('data.entries', data.entries);
-  // console.log('data.nextEntryId', data.nextEntryId);
 });
