7024dc2a427e7eef687252a08128faa1b759d69a
Cristofer Martinez <cristoferm@gmail.com>
2021-05-25 10:51:47 -0700

    Listen for submit events on the journal entry form and:

--- js/main.js
+++ js/main.js
@@ -1,13 +1,43 @@
 /* global data */
 /* exported data */
 
-// var $form = document.querySelector('.newEntry');
 var $photoURL = document.querySelector('.photoURL');
 var $newEntryImg = document.querySelector('.newEntryImg');
 
-// console.log($form.elements);
-// console.log($photoURL);
+// console.log($form.elements.target);
+// console.dir($form);
+// console.log($submit);
 
 $photoURL.addEventListener('input', function (event) {
   $newEntryImg.setAttribute('src', event.target.value);
+  if (event.target.value.length === 0) { $newEntryImg.setAttribute('src', 'images/placeholder-image-square.jpg'); }
+});
+
+var $form = document.querySelector('.newEntry');
+
+var $submit = document.querySelector('.button');
+$submit.addEventListener('click', function (event) {
+  event.preventDefault();
+  var title = document.querySelector('.title').value;
+  var photoURL = document.querySelector('.photoURL').value;
+  var notes = document.querySelector('.notes').value;
+
+  var newEntry = {
+    title: title,
+    photoURL: photoURL,
+    notes: notes,
+    ID: data.nextEntryId
+  };
+
+  data.entries.unshift(newEntry);
+  data.nextEntryId++;
+
+  $form.reset();
+  $newEntryImg.setAttribute('src', 'images/placeholder-image-square.jpg');
+
+  localStorage.setItem('data', data);
+  localStorage.setItem('entries', data.entries);
+
+  // console.log('data.entries', data.entries);
+  // console.log('data.nextEntryId', data.nextEntryId);
 });
